c88e94fdedc999046c530caccb4f424511e8daa8
NeonPikachu17 <msbernabe1@up.edu.ph>
2020-02-28 02:40:07 +0800

    style: fix format of finish stage

--- app/src/main/java/com/example/bernabe_exer4_slambook/MainActivity.kt
+++ app/src/main/java/com/example/bernabe_exer4_slambook/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.bernabe_exer4_slambook
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.databinding.DataBindingUtil
 import com.example.bernabe_exer4_slambook.databinding.ActivityMainBinding
 
@@ -16,20 +18,67 @@ class MainActivity : AppCompatActivity() {
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.slamBook = slamBook
+        binding.name.visibility = View.GONE
+        binding.nickname.visibility = View.GONE
+        binding.age.visibility = View.GONE
+        binding.goodbye.visibility = View.GONE
         binding.doneButton.setOnClickListener{
             setData(it)
         }
     }
 
     private fun setData(view: View){
+        // Assigns value to specific item in slamBook data
         binding.apply{
             slamBook?.name = editName.text.toString()
+            slamBook?.nickname = editNickname.text.toString()
+            slamBook?.age = editText4.text.toString()
+            slamBook?.birthday = editDate.text.toString()
+            slamBook?.course = editCourse.text.toString()
+            slamBook?.phone = editPhone.text.toString()
+            slamBook?.color = editColor.text.toString()
+            slamBook?.dream = editDream.text.toString()
+            slamBook?.crush = editCrush.text.toString()
+            slamBook?.message = editMsg.text.toString()
             invalidateAll()
         }
+        setVisibility()
+        // Hide the keyboard.
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
     }
 
     private fun setVisibility(){
-
+        binding.apply{
+            title.visibility = View.GONE
+            nameTitle.visibility = View.GONE
+            editName.visibility = View.GONE
+            titleNickname.visibility = View.GONE
+            editNickname.visibility = View.GONE
+            titleAge.visibility = View.GONE
+            editText4.visibility = View.GONE
+            titleBirthday.visibility = View.GONE
+            editDate.visibility = View.GONE
+            titleCourse.visibility = View.GONE
+            editCourse.visibility = View.GONE
+            titleCourse.visibility = View.GONE
+            editCourse.visibility = View.GONE
+            titlePhone.visibility = View.GONE
+            editPhone.visibility = View.GONE
+            titleColor.visibility = View.GONE
+            editColor.visibility = View.GONE
+            titleDream.visibility = View.GONE
+            editDream.visibility = View.GONE
+            titleCrush.visibility = View.GONE
+            editCrush.visibility = View.GONE
+            titleMsg.visibility = View.GONE
+            editMsg.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            name.visibility = View.VISIBLE
+            nickname.visibility = View.VISIBLE
+            age.visibility = View.VISIBLE
+            goodbye.visibility = View.VISIBLE
+        }
     }
 
 }
--- app/src/main/java/com/example/bernabe_exer4_slambook/SlamBook.kt
+++ app/src/main/java/com/example/bernabe_exer4_slambook/SlamBook.kt
@@ -1,5 +1,5 @@
 package com.example.bernabe_exer4_slambook
 
 data class SlamBook(var greeting: String = "", var name: String = "", var nickname: String = "", var age: String = "",
-                    var birthday: String = "", var course: String = "", var color: String = "",
-                    var dream: String = "", var crush: String = "", var message: String = "")
+                    var birthday: String = "", var course: String = "", var phone: String = "", var color: String = "",
+                    var dream: String = "", var crush: String = "", var message: String = "", var goodbye: String = "Thank you for answering!")
